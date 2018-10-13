a4f8bf5ec444888910d16d31790894a4d12a7aa2
Connor Moody <cdmoody0604@gmail.com>
2018-10-13 14:26:19 -0600

    set up utils tests

---
+++ test/UtilsTest.cpp
@@ -0,0 +1,19 @@
+#ifndef UTILSTEST_H_
+#define UTILSTEST_H_
+
+#include "./TestSuite.h"
+
+namespace
+{
+
+class UtilsTest: public TestSuite
+{
+public:
+    UtilsTest();
+private:
+
+};
+
+}
+
+#endif
